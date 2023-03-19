da55bcd6f3a3063743ed0cf442be624873173e57
olenahnyp <116520570+olenahnyp@users.noreply.github.com>
2023-03-19 20:39:06 +0200

    Update Main_Form.js

--- src/components/Main_Form.js
+++ src/components/Main_Form.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import "../styles/Main_form.css"
 import axios from 'axios';
 import Form_reg from './Form_reg';
+import  { MapComponent } from './Map.js';
 function Main_Form() {
     const { id_coded } = useParams();
     const [loading, setLoading] = useState(true)
@@ -17,13 +18,20 @@ function Main_Form() {
     }, []);
     return (
         <div className='main_form'>
-            {loading ?<div class="loader_block"><div class="loader"></div><p>Wait a little</p></div>:
+            {loading ?<div class="loader_block"><div class="loader"></div><p>Зачекайте</p></div>:
                 <>
                     {response ? <>
+                    <div className='reg_text_div' >Реєстрація кімнати {response.name}</div>
+
                     <div className='reg_text_div' >Ви реєструєте кімнату №{response.number}</div>
+
                     <Form_reg room={response} id_coded={id_coded}  />
+                    <div className="greeting">
+                        <strong><p>Вітаємо вдома!</p></strong>
+                        <MapComponent />
+                    </div>
                     </> : <>
-                    <h1>Sorry your code is invalid</h1>
+                    <h1>Виникла помилка</h1>
                     </>}
                 </>}
 
